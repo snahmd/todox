5c3132de531b44a46f13edd0911628fa58029660
San <sanahmed7@gmail.com>
2025-01-17 23:41:51 +0100

    refactor: reorganize dashboard and todos structure, add loading and not-found components

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+      <main>
+          <h3>Login</h3>
+      </main>
+    )
+  }
---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,8 @@
+
+  export default function Signup() {
+    return (
+          <main>
+              <h3>Signup</h3>
+          </main>
+        )
+    }
