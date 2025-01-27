8187a65efd53e47443dbac5541225ecc2d208a32
San <sanahmed7@gmail.com>
2025-01-27 23:50:21 +0100

    refactor: remove old todos API and implement new dynamic route for individual todo fetching

--- app/api/todos/root.js
+++ app/api/todos/root.js
@@ -11,3 +11,20 @@ export async function GET() {
     status: 200
   })
 }
+
+
+export async function POST(request) {
+  const todo = await request.json()
+
+  const res = await fetch('http://localhost:4000/todos', {
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(todo)
+  })
+
+  const yeniTodo = await res.json()
+
+  return NextResponse.json(yeniTodo, {
+    status: 201
+  })
+}
